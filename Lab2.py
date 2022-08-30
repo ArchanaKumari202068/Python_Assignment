f1d3a1fca0c0d439dada1228d1f1d332ba607b16
Archana bhardwaj <ArchanaKumari202068@gmail.com>
2022-08-30 22:21:06 +0530

    Lab2 file updated

--- Lab2.py
+++ Lab2.py
@@ -1,6 +1,7 @@
 #Lab-2: Write a function that returns all the permutation of the given string of length 3.
 
-word = "XYZ"
+word = input('Enter three letter word : ')
+
 result = []
 for i in range(len(word)):
     for j in range(len(word)):
@@ -10,4 +11,6 @@ for i in range(len(word)):
             if  j==k or i==k:
                 continue
             result.append(word[i]+word[j]+word[k])
+
+print('All letter permutations of the XYZ are : ')            
 print(result)
